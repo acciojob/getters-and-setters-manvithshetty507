aa7ccb0cd293b29802bd8fdb568266d3909c8ce2
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2023-07-20 00:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 //complete this code
-class Person {}
+class Person {
+	consrtuctor(name, age) {
+		this.name = name;
+		setAge(age);
+	}
+	getName() => this.name;
+	setAge(age) => {this.age = age};
 
-class Student extends Person {}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	study() => {
+		console.log(getName() + ' is studying');
+	}
+}
+
+class Teacher extends Person {
+	teach() => {
+		console.log(getName() + ' is teachimg')
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
